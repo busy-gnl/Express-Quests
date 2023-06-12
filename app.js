9c298dc01366e00e3758ad3bb60308913260ac92
busy-gnl <vallantjesse@live.com>
2023-06-12 21:10:27 +0200

    quest 02 completed

--- app.js
+++ app.js
@@ -1,18 +1,52 @@
-const express = require("express");
+require("dotenv").config();
+const { APP_PORT} = process.env;
 
+const express = require("express");
 const app = express();
 
-const port = 5000;
+const port = APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send("Welcome to my favorite movie list");
 };
+const users = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Jane" },
+];
+
+// Routes handlers
 
 app.get("/", welcome);
+app.get("/api/users", (req, res) => {
+  res.status(200).json(users);
+});
+app.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  const user = users.find((user) => user.id === id);
+  if (!user){
+    return res.status(404).send("User not found" );
+  }
+  res.status(200).json(user);
+});
+
 
 const movieHandlers = require("./movieHandlers");
+const database = require("./database");
 
-app.get("/api/movies", movieHandlers.getMovies);
+const getMovies = (req, res) => {
+  database
+    .query("select * from movies")
+    .then(([movies]) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+app.get("/api/movies", getMovies());
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
 app.listen(port, (err) => {
---
+++ database.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+const mysql = require("mysql2/promise");
+
+const database = mysql.createPool({
+    host: DB_HOST, // address of the server
+    port: DB_PORT, // port of the DB server (mysql), not to be confused with the APP_PORT !
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+  });
+  
+  database
+    .getConnection()
+    .then(() => {
+      console.log("Can reach database");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+//   database
+//   .query("select * from movies")
+//   .then(([movies]) => {
+//     console.log(movies);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
+
+module.exports = database;
